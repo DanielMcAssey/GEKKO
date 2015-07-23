969f47db97d8dad48da20a7cc3e611933bbd3e81
Daniel McAssey <daniel.mcassey@glokon.com>
2015-07-23 23:27:47 +0100

    Initial code

---
+++ source/.example.env.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+
+	// General Config
+	'ENCRYPTION_KEY'			=> '', // Random 32 character string
+
+	// Database Config
+	'DB_DRIVER'					=> '', // sqlite, mysql, pgsql, sqlsrv
+	'DB_HOST'					=> '',
+	'DB_NAME'					=> '',
+	'DB_USERNAME'				=> '',
+	'DB_PASSWORD'				=> '',
+	'DB_PREFIX'					=> '',
+
+);
+
+?>
