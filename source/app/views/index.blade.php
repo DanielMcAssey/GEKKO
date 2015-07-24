f601dac0a0c004d16bf5e1c517994973289684d2
Daniel McAssey <daniel.mcassey@glokon.com>
2015-07-24 18:32:47 +0100

    Remove specific defaults

--- source/app/views/index.blade.php
+++ source/app/views/index.blade.php
@@ -1,4 +1,4 @@
-<h1>GEKKO URL Shortener Service [domain: dcm.li]</h1>
+<h1>GEKKO URL Shortener Service</h1>
 <br /><br />
 MANAGE: <a href="/manage/">Manage your short links</a><br />
 ABUSE: <a href="mailto:hello@glokon.me?subject=GEKKO%20Support">Report Abuse</a>
