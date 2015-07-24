221cf3c53be11c88aae373de4d59b951bec9eea2
Daniel McAssey <daniel.mcassey@glokon.com>
2015-07-24 04:36:12 +0100

    Fix index, bump version

--- source/app/views/index.blade.php
+++ source/app/views/index.blade.php
@@ -1,1 +1,5 @@
-TEMP INDEX
+<h1>GEKKO URL Shortener Service [domain: dcm.li]</h1>
+<br /><br />
+MANAGE: <a href="/manage/">Manage your short links</a><br />
+ABUSE: <a href="mailto:hello@glokon.me?subject=GEKKO%20Support">Report Abuse</a>
+<!-- Ill make this page look nicer at some point :p -->
